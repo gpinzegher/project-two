bd82d5dbb1bb16213d4f5ae7975eaa075d460b64
Guilherme Pinzegher <guilherme@pzg.com.br>
2022-06-16 09:55:19 -0400

    Project update

--- src/main/java/br/com/pzg/project/two/ProjectTwoApplication.java
+++ src/main/java/br/com/pzg/project/two/ProjectTwoApplication.java
@@ -2,8 +2,6 @@ package br.com.pzg.project.two;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProjectTwoApplication {
 
